9424994827d8ee398696bbc0fc6aa80a221e5a4e
Luca Palummo <80684249+Grimm2577@users.noreply.github.com>
2024-09-19 21:59:50 +0200

    Add files via upload
    Day 1 of the 100 Days of Code: The Complete Python Pro Bootcamp
    Project Number 1: Create a band name generator
    
    My submission attempt

---
+++ band_name_generator.py
@@ -0,0 +1,20 @@
+# importing colorama to add some color to the terminal text
+from colorama import Fore
+
+# adding a greeting to the program
+intro = "Welcome To Band Name Generator."
+
+# printing the intro and setting its color to green
+print(Fore.GREEN + intro)
+
+# receiving and storing the city name into a variable and setting the question text color to white
+childhood_city = input(Fore.WHITE + "In what city did you grow up?\n")
+
+# receiving and storing the pet name into a variable and setting the question text color to white
+pet_name = input(Fore.WHITE + "What is your pets name\n")
+
+# creating a band name string and concatenating the 2 previous variables
+band_name = childhood_city + " " + pet_name
+
+# printing the final out put and setting the band name text color to cyan
+print("Your band name could be: " + Fore.CYAN + band_name)
